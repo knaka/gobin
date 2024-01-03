ebfe2c7305985f39fb71f2db40cba7cbdcb64190
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-01-03 20:38:40 +0900

    Renamed

--- result.go
+++ result.go
@@ -7,80 +7,74 @@ func Ok[T any](value T) (T, error) {
 
 func empty[T any]() (t T) { return }
 
-// Error returns a zero value and the given error.
-func Error[T any](err error) (T, error) {
+// Err returns a zero value and the given error.
+func Err[T any](err error) (T, error) {
 	return empty[T](), err
 }
 
-// Then calls the given procedure if err is nil.
-func Then(err error, fn func()) {
-	if err == nil {
-		fn()
-	}
-}
-
-// Bind returns the result of the given function if err is nil, otherwise the error.
-func Bind[T any](err error, fn func() T) T {
+// Let returns the result of the given function if err is nil, otherwise the error.
+func Let[T any](err error, fn func() T) T {
 	if err != nil {
 		return empty[T]()
 	}
 	return fn()
 }
 
-// BindErr returns the result of the given function that can fail if err is nil, otherwise the error.
-func BindErr[T any](err error, fn func() (T, error)) (T, error) {
+// Bind returns the result of the given function that can fail if err is nil, otherwise the error.
+func Bind[T any](err error, fn func() (T, error)) (T, error) {
 	if err != nil {
 		return empty[T](), err
 	}
 	return fn()
 }
 
-// ----------------------------------------------------------------
-
-// Err is an alias of Error.
-func Err[T any](err error) (T, error) {
-	return empty[T](), err
-}
-
-// Bind0 is an alias of Then.
-func Bind0(err error, fn func()) {
-	if err != nil {
-		return
-	}
-	fn()
-}
-
-// Bind1 is an alias of Bind.
-func Bind1[T any](err error, fn func() T) T {
-	if err != nil {
-		return empty[T]()
-	}
-	return fn()
-}
-
-func Bind2[T any, U any](err error, fn func() (T, U)) (T, U) {
-	if err != nil {
-		return empty[T](), empty[U]()
-	}
-	return fn()
-}
-
-func BindErr0(err error, fn func() error) error {
+// Then calls the given function if err is nil.
+func Then(err error, fn func() error) error {
 	if err != nil {
 		return err
 	}
 	return fn()
 }
 
-// BindErr1 is an alias of BindErr.
-func BindErr1[T any](err error, fn func() (T, error)) (T, error) {
+// Let0 calls the given function if err is nil and returns nothing.
+func Let0(err error, fn func()) {
+	if err != nil {
+		return
+	}
+	fn()
+}
+
+// Let1 is an alias of Let.
+func Let1[T any](err error, fn func() T) T {
+	if err != nil {
+		return empty[T]()
+	}
+	return fn()
+}
+
+func Let2[T any, U any](err error, fn func() (T, U)) (T, U) {
+	if err != nil {
+		return empty[T](), empty[U]()
+	}
+	return fn()
+}
+
+func Bind0(err error, fn func() error) error {
+	if err != nil {
+		return err
+	}
+	return fn()
+}
+
+// Bind1 is an alias of Bind.
+func Bind1[T any](err error, fn func() (T, error)) (T, error) {
 	if err != nil {
 		return empty[T](), err
 	}
 	return fn()
 }
 
-func BindErr2[T any, U any](err error, fn func() (T, U, error)) (T, U, error) {
+func Bind2[T any, U any](err error, fn func() (T, U, error)) (T, U, error) {
 	if err != nil {
 		return empty[T](), empty[U](), err
 	}
