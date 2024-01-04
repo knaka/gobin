e4daa7d8b39474053e602041264453a9140517b5
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-01-04 18:15:30 +0900

    Refactor

--- debugger_waiter.go
+++ debugger_waiter.go
@@ -18,17 +18,22 @@ func WaitForDebugger() {
 		return
 	}
 	pid := os.Getpid()
-	Assert(fmt.Fprintf(os.Stderr, "Process %d is waiting\n", pid))
+	_, _ = fmt.Fprintf(os.Stderr, "Process %d is waiting\n", pid)
 outer:
 	for {
 		time.Sleep(1 * time.Second)
 		lines := (func() string {
+			var err error
 			command := exec.Command("ps", "w")
-			stdout := Ensure(command.StdoutPipe())
-			defer (func() { Ignore(stdout.Close()) })()
-			Assert(command.Start())
-			defer (func() { Assert(command.Wait()) })()
-			return string(Ensure(io.ReadAll(stdout)))
+			stdout, err := command.StdoutPipe()
+			defer Let0(err, func() { _ = stdout.Close() })
+			err = Then(err, func() error { return command.Start() })
+			defer Let0(err, func() { _ = command.Wait() })
+			data, err := Bind(err, func() ([]byte, error) { return io.ReadAll(stdout) })
+			if err != nil {
+				panic(err)
+			}
+			return string(data)
 		})()
 		for _, line := range strings.Split(lines, "\n") {
 			if strings.Contains(line, "dlv") &&
@@ -37,6 +42,6 @@ outer:
 			}
 		}
 	}
-	Assert(fmt.Fprintf(os.Stderr, "Debugger connected"))
+	_, _ = fmt.Fprintf(os.Stderr, "Debugger connected")
 	time.Sleep(1 * time.Second)
 }
