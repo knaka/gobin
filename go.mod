d2188d1cc0853e82680b35c62d918e76ceb8f96c
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-06-28 13:00:22 +0900

    Refactoring

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module github.com/knaka/go-run-cache
 
 go 1.20
 
-require golang.org/x/mod v0.14.0
+require (
+	github.com/knaka/go-utils v0.0.2024030337
+	github.com/samber/lo v1.42.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/mod v0.17.0
+)
+
+require (
+	github.com/friendsofgo/errors v0.9.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,95 @@
 package main
 
 import (
-	"github.com/knaka/go-run-cache/lib"
+	"bufio"
+	"fmt"
+	"github.com/samber/lo"
+	"go.uber.org/zap"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+
+	. "github.com/knaka/go-utils"
 )
 
+func help() {
+	V0(fmt.Fprintf(os.Stderr, "Usage: %s <subcommand> [options]\n", os.Args[0]))
+}
+
+var fileBaseList = []string{
+	"Gofile",
+	"Gobinfile",
+	".Gofile",
+	".Gobinfile",
+}
+
+type Gobin struct {
+	pkg     string
+	tags    string
+	comment string
+}
+
+func gobinApply(args []string) (err error) {
+	defer Catch(&err)
+	filePathList := lo.FilterMap(fileBaseList, func(fileBase string, _ int) (string, bool) {
+		filePath := filepath.Join(V(os.UserHomeDir()), fileBase)
+		stat, err := os.Stat(filePath)
+		if err != nil {
+			return "", false
+		}
+		if stat.IsDir() {
+			return "", false
+		}
+		return filePath, true
+	})
+	sugar := logger.Sugar()
+	sugar.Infof("filePathList: %+v", filePathList)
+	if len(filePathList) == 0 {
+		err = fmt.Errorf("no gobin list file found")
+		return
+	}
+	filePath := filePathList[0]
+	var gobinList []Gobin
+	// Reead file line by line
+	scanner := bufio.NewScanner(V(os.Open(filePath)))
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		divs := strings.SplitN(line, "#", 2)
+		pkg := strings.TrimSpace(divs[0])
+		if pkg == "" {
+			continue
+		}
+		comment := TernaryF(len(divs) >= 2, func() string { return strings.TrimSpace(divs[1]) }, nil)
+		gobinList = append(gobinList, Gobin{pkg: pkg, comment: comment})
+	}
+	sugar.Infof("gobinList: %+v", gobinList)
+	return
+}
+
+var logger = zap.NewNop()
+
 func main() {
-	err := lib.Run(os.Args[1:])
+	WaitForDebugger()
+	if os.Getenv("VERBOSE") != "" {
+		logger = V(zap.NewDevelopment())
+	}
+	var err error
+	if len(os.Args) < 2 {
+		help()
+		os.Exit(1)
+	}
+	subCmd := os.Args[1]
+	args := os.Args[2:]
+	switch subCmd {
+	case "apply":
+		err = gobinApply(args)
+	}
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Fatalf("Error: %+v", err)
 	}
 }
