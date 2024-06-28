dea67a04c65abbb7af97edc1c2d2ae359b5ac98b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-06-28 23:01:32 +0900

    Refactor

--- go.mod
+++ go.mod
@@ -6,13 +6,15 @@ require (
 	github.com/knaka/go-utils v0.0.2024030337
 	github.com/samber/lo v1.42.0
 	go.uber.org/zap v1.27.0
-	golang.org/x/mod v0.17.0
+	golang.org/x/mod v0.18.0
 )
 
 require (
 	github.com/friendsofgo/errors v0.9.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	. "github.com/knaka/go-utils"
 	"github.com/samber/lo"
 	"go.uber.org/zap"
 	"log"
 	"os"
+	"os/exec"
+	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
-
-	. "github.com/knaka/go-utils"
+	"sync"
 )
 
 func help() {
@@ -26,11 +30,16 @@ var fileBaseList = []string{
 
 type Gobin struct {
 	pkg     string
+	ver     string
 	tags    string
 	comment string
 }
 
-func gobinApply(args []string) (err error) {
+type GoListOutput struct {
+	Version string `json:"Version"`
+}
+
+func getGobinList() (gobinList []Gobin, err error) {
 	defer Catch(&err)
 	filePathList := lo.FilterMap(fileBaseList, func(fileBase string, _ int) (string, bool) {
 		filePath := filepath.Join(V(os.UserHomeDir()), fileBase)
@@ -43,15 +52,12 @@ func gobinApply(args []string) (err error) {
 		}
 		return filePath, true
 	})
-	sugar := logger.Sugar()
 	sugar.Infof("filePathList: %+v", filePathList)
 	if len(filePathList) == 0 {
 		err = fmt.Errorf("no gobin list file found")
 		return
 	}
 	filePath := filePathList[0]
-	var gobinList []Gobin
-	// Reead file line by line
 	scanner := bufio.NewScanner(V(os.Open(filePath)))
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -60,23 +66,119 @@ func gobinApply(args []string) (err error) {
 			continue
 		}
 		divs := strings.SplitN(line, "#", 2)
-		pkg := strings.TrimSpace(divs[0])
-		if pkg == "" {
+		pkgVerTags := strings.TrimSpace(divs[0])
+		if pkgVerTags == "" {
 			continue
 		}
 		comment := TernaryF(len(divs) >= 2, func() string { return strings.TrimSpace(divs[1]) }, nil)
-		gobinList = append(gobinList, Gobin{pkg: pkg, comment: comment})
+		divs = strings.SplitN(pkgVerTags, ",", 2)
+		pkgVer := divs[0]
+		tags := TernaryF(len(divs) >= 2, func() string { return divs[1] }, nil)
+		divs = strings.SplitN(pkgVer, "@", 2)
+		pkg := divs[0]
+		ver := TernaryF(len(divs) >= 2, func() string { return divs[1] }, func() string { return "latest" })
+		if ver == "latest" {
+			divs = strings.Split(pkg, "/")
+			module := fmt.Sprintf("%s/%s/%s", divs[0], divs[1], divs[2])
+			divs = divs[3:]
+			for {
+				cmd := exec.Command("go", "list", "-m", "--json", fmt.Sprintf("%s@%s", module, ver))
+				cmd.Env = append(os.Environ(), "GO111MODULE=on")
+				goListOutput := GoListOutput{}
+				V0(json.Unmarshal(V(cmd.Output()), &goListOutput))
+				if !strings.HasSuffix(goListOutput.Version, "+incompatible") {
+					ver = goListOutput.Version
+					break
+				}
+				if len(divs) == 0 {
+					return nil, fmt.Errorf("no version found for %s", pkg)
+				}
+				module = fmt.Sprintf("%s/%s", module, divs[0])
+				divs = divs[1:]
+			}
+		}
+		gobinList = append(gobinList, Gobin{
+			pkg:     pkg,
+			ver:     ver,
+			tags:    tags,
+			comment: comment})
 	}
 	sugar.Infof("gobinList: %+v", gobinList)
+	return gobinList, nil
+}
+
+// GoEnv is a struct to hold the output of `go env -json`.
+type GoEnv struct {
+	Version string `json:"GOVERSION"`
+	Gobin   string `json:"GOBIN"`
+	Gopath  string `json:"GOPATH"`
+}
+
+var goVersion = sync.OnceValues(func() (goVersion string, err error) {
+	defer Catch(&err)
+	return V(goEnv()).Version, nil
+})
+
+var goBin = sync.OnceValues(func() (goBin string, err error) {
+	defer Catch(&err)
+	goBin = V(goEnv()).Gobin
+	if goBin != "" {
+		return goBin, nil
+	}
+	return filepath.Join(V(goEnv()).Gopath, "bin"), nil
+})
+
+var goCmd = sync.OnceValues(func() (goPath string, err error) {
+	defer Catch(&err)
+	goPath = filepath.Join(runtime.GOROOT(), "bin", "go")
+	if stat, err := os.Stat(goPath); err == nil && !stat.IsDir() {
+		return goPath, nil
+	}
+	goPath, err = exec.LookPath("go")
+	if err == nil {
+		return goPath, nil
+	}
+	return "", fmt.Errorf("go command not found")
+})
+
+var goEnv = sync.OnceValues(func() (goEnv_ GoEnv, err error) {
+	defer Catch(&err)
+	outStr := V(exec.Command(V(goCmd()), "env", "-json").Output())
+	V0(json.Unmarshal(outStr, &goEnv_))
 	return
+})
+
+func gobinApply(_ []string) (err error) {
+	defer Catch(&err)
+	gobinList := V(getGobinList())
+
+	for _, gobin := range gobinList {
+		if stat, err := os.Stat(filepath.Join(V(goBin()), fmt.Sprintf("%s@%s", path.Base(gobin.pkg), gobin.ver))); err == nil && !stat.IsDir() {
+			sugar.Infof("Skipping %s@%s", gobin.pkg, gobin.ver)
+			continue
+		}
+		args := []string{"install"}
+		if gobin.tags != "" {
+			args = append(args, "-tags", gobin.tags)
+		}
+		args = append(args, fmt.Sprintf("%s@%s", gobin.pkg, gobin.ver))
+		//goCmd := V(goCmd())
+		goCmd := "echo"
+		cmd := exec.Command(goCmd, args...)
+		cmd.Stdout = os.Stdout
+		V0(cmd.Run())
+	}
+	return nil
 }
 
 var logger = zap.NewNop()
+var sugar = logger.Sugar()
 
 func main() {
 	WaitForDebugger()
 	if os.Getenv("VERBOSE") != "" {
 		logger = V(zap.NewDevelopment())
+		sugar = logger.Sugar()
 	}
 	var err error
 	if len(os.Args) < 2 {
