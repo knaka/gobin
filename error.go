d4dfc9451294ee0c5e3a3637cb2276f5ba84f3b9
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-07-02 15:18:35 +0900

    Refactor

--- error.go
+++ error.go
@@ -29,6 +29,14 @@ func V2[T any, U any](value1 T, value2 U, err error) (T, U) {
 	return value1, value2
 }
 
+// V3 returns three values. If err is not nil, it panics.
+func V3[T any, U any, V any](value1 T, value2 U, value3 V, err error) (T, U, V) {
+	if err != nil {
+		panic(wrapWithStack(err))
+	}
+	return value1, value2, value3
+}
+
 // V0 returns no value. If err is not nil, it panics.
 //
 //goland:noinspection GoUnusedExportedFunction, GoUnnecessarilyExportedIdentifiers
