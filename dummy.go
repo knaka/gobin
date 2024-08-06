e542b604022781f4737173adef82a41e3ef2b798
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-08-06 13:03:56 +0900

    Refactor

--- dummy.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-// +build tools
-
-package main
-
-// Go Wiki: Go Modules - The Go Programming Language https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-
-import _ "golang.org/x/tools/cmd/stringer"
