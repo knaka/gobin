b06c601023ea83bcc71e59d686a83f8ea95bdcb2
Kiichiro NAKA <knaka@ayutaya.com>
2024-09-29 15:06:49 +0900

    Add task scripts

--- task-project.lib.sh
+++ task-project.lib.sh
@@ -2,3 +2,10 @@
 set -o nounset -o errexit
 
 test "${guard_0416bc0+set}" = set && return 0; guard_0416bc0=-
+
+. task.sh
+. task-go.lib.sh
+
+task_gen() (
+  cross_run ./cmd-gobin run go-generate-fast .
+)
